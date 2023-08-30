7c36d945d8c5eeeadc1ee222b41274ae363426a0
Alexandr Dubinin <akiyamka@gmail.com>
2023-08-30 20:13:50 +0200

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-version=$(git tag | grep -E '^[0-9]' | sort -V | tail -1)
+version=$(git tag | tail -1)
 flutter build apk
 cp ./build/app/outputs/flutter-apk/app-release.apk ./releases/live-sensors-${version}-release.apk
