67c2037185959ce929e311aeea790642f3df66b2
Alexandr Dubinin <akiyamka@gmail.com>
2023-08-30 14:42:36 +0200

    ci: artifacts build logic

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 version=$(git tag | grep -E '^[0-9]' | sort -V | tail -1)
 flutter build apk --dart-define-from-file=.env.json
-mv ./build/app/outputs/flutter-apk/app-release.apk ./releases/live-sensors-${version}-release.apk
+cp ./build/app/outputs/flutter-apk/app-release.apk ./releases/live-sensors-${version}-release.apk
