3d72a0e9ef54fa6a276e212421eb9cd5361cefae
Palina Krukovich <palina@Palinas-MacBook-Pro.local>
2024-01-25 14:46:39 +0100

    Update paths in release.sh script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/bin/bash ./ask_and_set_version.sh
-/bin/bash ./build.sh
+/bin/bash ./scripts/ask_and_set_version.sh
+/bin/bash ./scripts/build.sh
 echo
 read -p "Release new version? (y/n): " -n 1 -r choice
 echo
